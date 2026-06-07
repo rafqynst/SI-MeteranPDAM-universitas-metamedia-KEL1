d468c2c8d1cd84bd86d5bba7f9abf7b2abe8770d
Nanda <nanda@gmail.com>
2026-06-07 13:48:51 +0700

    tambah fitur tagihan

--- pelanggan.php
+++ pelanggan.php
@@ -438,11 +438,10 @@ class="fixed inset-0 bg-black/50 hidden items-center justify-center z-50">
 
                                                 </button>
 
-                                                <a href="edit_pelanggan.php?id=<?= $data['id_pelanggan']; ?>"
+                                                <a href="detail_tagihan.php"
                                                     class="bg-green-600 hover:bg-green-800 text-white px-2 py-2 rounded-lg">
 
                                                     <i class="fas fa-file-invoice-dollar"></i>
-
                                                 </a>
 
                                             </div>
